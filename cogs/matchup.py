69470af0344c7e66eccf370de1394728f723e256
Sam <smadler92@gmail.com>
2020-01-22 21:01:25 -0600

    Update matchup.py

--- cogs/matchup.py
+++ cogs/matchup.py
@@ -22,27 +22,27 @@ class Matchup(commands.Cog):
         else:
             msg=""
             mono=False
+            mt=""
             if arg2==None:
                 #no 2nd argument
                 types=arg1.split("/")
                 if len(types)==1:
-                    ptype =str(types[0].capitalize())
-                    ptype+="/"+str(types[0].capitalize())
+                    mt=str(types[0]).capitalize()
+                    ptype =mt + "/" + mt
+                    mono=True
                 else:
                     ptype = str(types[0].capitalize())
                     ptype+="/"+str(types[1].capitalize())
-#                else:
- #                   #for some reason it never gets here, should be for monotypes
-  #                  ptype = str(arg1.capitalize())
-   #                 ptype+="/"+str(arg1.capitalize())
-    #                msg+="Success"
             else:
                 ptype = str(arg1.capitalize())
                 ptype+="/"+str(arg2.capitalize())
 
             if (ptype not in self.types_dict):
                 msg += "Invalid input!"
-
+            elif (mono=True):
+                msg+=' For ' + str(mt) +' types, use '
+                msg+='\n'.join(map(str, self.types_dict.get(ptype))).rstrip()
+                msg+=' type moves!'
             else:
                 msg+=' For ' + str(ptype) +' types, use '
                 msg+='\n'.join(map(str, self.types_dict.get(ptype))).rstrip()
