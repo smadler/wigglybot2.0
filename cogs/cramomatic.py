87ddcfbcc741f6ec95e8e471811e15cee0fdec40
Eldaste <oliver.m.wimmer@vanderbilt.edu>
2020-07-09 04:22:50 -0500

    primary attempt recipie

--- cogs/cramomatic.py
+++ cogs/cramomatic.py
@@ -5,6 +5,7 @@ from .utils import cramData
 import re
 import functools
 import collections
+import random
 
 class Cramomatic(commands.Cog):
 
@@ -52,6 +53,21 @@ class Cramomatic(commands.Cog):
             await ctx.send("I don't know how to make that.")
             return
 
+        dataname = self.recipieindex[int(workingr.lastgroup[1:])]
+        data = self.recipies[dataname]
+
+        target = random.choice(data)
+
+        if target[0] == 'Special':
+            await ctx.send("To make %s, toss `%s` into the Cram-O-Matic." % (dataname, target[1] + ', ' + random.choice(self.ingredients.keys()) + ', ' + target[1] + ', ' + target[1]))
+            return
+
+        resultant = self.findrecipe(4, [], 0, self.expandValue(target[1]), target[0])
+        if resultant == None:
+            resultant = ['ERROR, No Recipe found']
+
+        await ctx.send("To make %s, toss `%s` into the Cram-O-Matic." % (dataname, ', '.join(resultant)))
+
     @commands.command()
     async def recipeinfo(self, ctx, *args):
         if not (ctx.channel.id == 647701301031075862 or get(ctx.message.author.roles, name="Max Host") or get(ctx.message.author.roles, name="Mods")):
@@ -71,7 +87,7 @@ class Cramomatic(commands.Cog):
             await ctx.send("%s is a special recipie with the core ingredient of %s." % (dataname, self.recipies[dataname][0][1]))
             return
             
-        await ctx.send("%s can be made with the following ingredient combinations:\n%s" % (dataname, '\n'.join('A weight of %d to %d with the %s sttribute.' %
+        await ctx.send("%s can be made with the following ingredient combination(s):\n%s" % (dataname, '\n'.join('A weight of %d to %d with the %s attribute.' %
                                                 (self.expandValue(datanum)[0], self.expandValue(datanum)[1], dataty) for dataty, datanum in self.recipies[dataname])))
 
     @commands.command()
@@ -161,11 +177,60 @@ class Cramomatic(commands.Cog):
         if value > 14 or value < 0:
             return None
         if value == 14:
-            return (151, 1000) # 1000 is used as an arbitrarily large number
+            return (151, 255) # 255 is used as an arbitrarily large number
         if value == 0:
             return (0, 20)
         res = value * 10 + 11;
         return (res, res + 9)
+
+    def findrecipe(self, remaininging: int, inglist, currentweight: int, weighttarget, nature = None):
+        if remaininging < 1: # Should not happen
+            return None
+        
+        minweight = weighttarget[0] - currentweight if remaininging == 1 else 0
+        maxweight = weighttarget[1] - currentweight
+
+        while not minweight > maxweight:
+            if remaininging == 1 and inglist[0] in self.specialvalues and inglist[0] == inglist[2]:
+                prohibits = inglist[0]
+            else:
+                prohibits = None            
+            selectfrom = self.find(minweight, maxweight, nature, prohibits)
+            if selectfrom == []:
+                return None
+            select = random.choice(selectfrom)
+            currw = select['Value'] + currentweight
+            tempings = inglist[:]
+            tempings.append(select['Name'])
+            
+            if remaininging == 1: # Base Case
+                if currw > weighttarget[1] or currw < weighttarget[0]: # Double check, neither should be the case
+                    return None
+                return tempings
+
+            possibleres = self.findrecipe(remaininging - 1, tempings, currw, weighttarget)
+
+            if possibleres != None: # Found a set of values that works
+                return possibleres
+
+            # Value found was too low
+            minweight = select['Value'] + 1
+            
+        return None # No value was big enough to work
+
+    def find(self, minweight, maxweight, nature = None, prohibits = None): # Returns a list of dicts corrosponding to the given restrictions
+        res = []
+        
+        for possibility in self.ingredients.values():
+            if nature != None and possibility['Type'] != nature:
+                continue
+            if prohibits == possibility['Name']:
+                continue
+            if possibility['Value'] > maxweight or possibility['Value'] < minweight:
+                continue
+            res.append(possibility)
+
+        return res
     
 
 def setup(bot):
