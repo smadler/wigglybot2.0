b853006523d44209c3aa1eb068e2b11e60ccadbd
Eldaste <oliver.m.wimmer@vanderbilt.edu>
2020-07-16 01:16:46 -0500

    Adding Smartrecipes

--- cogs/cramomatic.py
+++ cogs/cramomatic.py
@@ -9,8 +9,12 @@ import random
 
 class Cramomatic(commands.Cog):
 
+    # Weighting Constants
+    WEIGHTINGCONSTANT = 4
+
     ### DICTIONARIES FOR ALL THE DATA NEEDED
     ingredients = cramData.getIngredients()
+    cramData.weightIngredients(ingredients)
     recipies = collections.defaultdict(list)
     recipieindex = []
     pivotrec = cramData.getResults()
@@ -38,6 +42,9 @@ class Cramomatic(commands.Cog):
         for key in self.recipies:
             self.recipieindex.append(key)
         self.recipieregex = re.compile('|'.join('(?P<K%d>%s)' % (key, self.recipieindex[key]) for key in range(len(self.recipieindex))), re.I)
+        self.quanta = self.composequanta()
+        while self.iteraterecipe():
+            pass
         
 
     @commands.command()
@@ -67,6 +74,28 @@ class Cramomatic(commands.Cog):
             resultant = ['ERROR, No Recipe found']
 
         await ctx.send("To make %s, toss `%s` into the Cram-O-Matic." % (dataname, ', '.join(resultant)))
+
+    @commands.command()
+    async def smartrecipe(self, ctx, *args):
+        if not (ctx.channel.id == 647701301031075862 or get(ctx.message.author.roles, name="Max Host") or get(ctx.message.author.roles, name="Mods")):
+            return
+
+        composed = ' '.join(args)
+
+        workingr = self.recipieregex.search(composed)
+
+        if workingr == None:
+            await ctx.send("I don't know how to make that.")
+            return
+
+        dataname = self.recipieindex[int(workingr.lastgroup[1:])]
+        data = self.recipies[dataname]
+
+        resultant = self.smartpicker(dataname, data)
+        if resultant == None:
+            resultant = ['ERROR, No Recipe found']
+
+        await ctx.send("To make %s, toss `%s` into the Cram-O-Matic." % (dataname, ', '.join(resultant[0])))
 
     @commands.command()
     async def recipeinfo(self, ctx, *args):
@@ -140,6 +169,24 @@ class Cramomatic(commands.Cog):
 
         await ctx.send("%s is a %s attribute item with a weight of %d." % (item, self.ingredients[item]['Type'], self.ingredients[item]['Value']))
 
+    @commands.command()
+    async def itemsubstitute(self, ctx, *args):
+        if not (ctx.channel.id == 647701301031075862 or get(ctx.message.author.roles, name="Max Host") or get(ctx.message.author.roles, name="Mods")):
+            return
+        
+        composed = ' '.join(args)
+        
+        ing = self.ingredienttokenizer.search(composed)
+
+        if ing == None:
+            await ctx.send("I don't know what that is.")
+            return
+        
+        item = re.sub('_', ' ', re.sub('__', '-', ing.lastgroup))
+
+        await ctx.send("You use %s as a replacement for any of the following:\n%s" % (item,
+                                '\n'.join(poss['Name'] for poss in self.find(self.ingredients[item]['Value'], self.ingredients[item]['Value'], None, [item]))))
+
     def modulateValue(self, value: int):
         if value < 1:
             return None
@@ -195,7 +242,7 @@ class Cramomatic(commands.Cog):
                 prohibits = inglist[0]
             else:
                 prohibits = None            
-            selectfrom = self.find(minweight, maxweight, nature, prohibits)
+            selectfrom = self.find(minweight, maxweight, nature, [prohibits])
             if selectfrom == []:
                 return None
             select = random.choice(selectfrom)
@@ -218,19 +265,105 @@ class Cramomatic(commands.Cog):
             
         return None # No value was big enough to work
 
-    def find(self, minweight, maxweight, nature = None, prohibits = None): # Returns a list of dicts corrosponding to the given restrictions
+    def find(self, minweight, maxweight, nature = None, prohibits = [], allowableweight = None): # Returns a list of dicts corrosponding to the given restrictions
         res = []
         
         for possibility in self.ingredients.values():
             if nature != None and possibility['Type'] != nature:
                 continue
-            if prohibits == possibility['Name']:
+            if allowableweight != None:
+                if not 'Weight' in possibility or possibility['Weight'] > allowableweight:
+                    continue
+            if possibility['Name'] in prohibits:
                 continue
             if possibility['Value'] > maxweight or possibility['Value'] < minweight:
                 continue
             res.append(possibility)
 
         return res
+
+    def composequanta(self):
+        res = []
+        for item in self.ingredients.values():
+            if item['Weight'] not in res:
+                res.append(item['Weight'])
+        res.sort()
+        return res
+
+    def iteraterecipe(self):
+        newvals = {}
+        for recipename in self.recipies:
+            name = "Kings Rock" if name == "King's Rock" else recipename
+            if name in self.ingredients:
+                xx = self.smartpicker(recipename, self.recipies[recipename], self.ingredients[name] - 1)
+                if xx != None:
+                    newvals[name] = xx[1] + self.WEIGHTINGCONSTANT
+        for key, val in newvals.items():
+            self.ingredients[key]['Weight'] = val
+        self.quanta = self.composequanta()
+        if newvals == {}:
+            return False
+        return True
+
+    def smartpicker(self, recipename, recipes, weightlimit = 300000):
+        minweight = weightlimit
+        res = None
+
+        for recipe in recipes:
+            if recipe[0] == 'Special':
+                randing = random.choice(self.find(0, 255, None, [recipename], self.quanta[0]))['Name']
+                return ([recipe[1], randing, recipe[1], recipe[1]], self.ingredients[recipe[1]['Weight']])
+            for quantum in self.quanta:
+                if quantum >= minweight:
+                    break
+                resultant = self.smartfindrecipe(4, [], 0, self.expandValue(recipe[1]), recipe[0], [recipename] if recipename != "King's Rock" else ["Kings Rock"], quantum)
+                if resultant != None:
+                    res = resultant
+                    minweight = quantum
+                    break
+
+        if res == None:
+            return None
+        return (res, quantum)
+    
+    def smartfindrecipe(self, remaininging: int, inglist, currentweight: int, weighttarget, nature = None, prohibited, quantum):
+        if remaininging < 1: # Should not happen
+            return None
+        
+        minweight = weighttarget[0] - currentweight if remaininging == 1 else 0
+        maxweight = weighttarget[1] - currentweight
+
+        for quantumm in self.quanta:
+            if quantumm > quantum:
+                return None
+            while not minweight > maxweight:
+                if remaininging == 1 and inglist[0] in self.specialvalues and inglist[0] == inglist[2]:
+                    prohibits = [inglist[0]]
+                else:
+                    prohibits = []
+                prohibits.extend(prohibited)
+                selectfrom = self.find(minweight, maxweight, nature, prohibits, quantumm)
+                if selectfrom == []:
+                    return None
+                select = random.choice(selectfrom)
+                currw = select['Value'] + currentweight
+                tempings = inglist[:]
+                tempings.append(select['Name'])
+                
+                if remaininging == 1: # Base Case
+                    if currw > weighttarget[1] or currw < weighttarget[0]: # Double check, neither should be the case
+                        return None
+                    return tempings
+
+                possibleres = self.smartfindrecipe(remaininging - 1, tempings, currw, weighttarget, prohibited, quantum)
+
+                if possibleres != None: # Found a set of values that works
+                    return possibleres
+
+                # Value found was too low
+                minweight = select['Value'] + 1
+            
+        return None # No value was big enough to work
     
 
 def setup(bot):
--- cogs/utils/cramData.py
+++ cogs/utils/cramData.py
@@ -7,6 +7,16 @@ def getIngredients():
                 result[' '.join(items[:-2])] = {"Value": int(items[-1]), "Type": items[-2], "Name": ' '.join(items[:-2])}
     return result
 
+def weightIngredients(result):
+    with open("./data/cramomatic/ingredientweights.txt", 'r') as f:
+        for line in f:
+            items = line.split()
+            if len(items) > 2:
+                itemname = ' '.join(items[:-1])
+                if itemname in result:
+                    result[itemname]['Weight'] = int(items[-1])
+    return result
+
 def getResults():
     result = {}
     with open("./data/cramomatic/recipies.txt", 'r') as f:
